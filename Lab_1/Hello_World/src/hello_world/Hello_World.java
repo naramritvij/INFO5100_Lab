da4ec2df41901501156a071e0a5c25efba6053d6
naramritvij <naram.r@northeastern.edu>
2024-09-13 14:45:40 -0400

    Added Hello_Workd in Java from Netbeans.

---
+++ Lab_1/Hello_World/src/hello_world/Hello_World.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello_world;
+
+/**
+ *
+ * @author admin
+ */
+public class Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
